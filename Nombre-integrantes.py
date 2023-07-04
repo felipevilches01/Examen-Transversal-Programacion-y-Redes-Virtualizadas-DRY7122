ffacdc3f7d195282ad48dbdcc229a12779a0fc9f
NetAcad DEVASC <changeme@example.com>
2023-07-04 23:07:03 +0000

    Examen Transversal

---
+++ Nombre-integrantes.py
@@ -0,0 +1,10 @@
+#Nombre de los integrantes de grupo 
+
+print ("Examen Transversal Programación y Redes Virtualizadas – DRY7122")
+print ("")
+print ("Los integrantes son:")
+print ("")
+print ("- Paulina Salinas")
+print ("- Constanza Urrutia")
+print ("- Felipe Vilches")
+
